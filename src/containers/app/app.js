2ca8b5e4f5a8f900415e57c1bae4b44f67489d13
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-10-20 17:35:43 -0500

    feat/folderStructure: set up folder structure

---
+++ src/containers/app/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h3>This is the home page broseidon.</h3>
+    </div>
+  );
+}
+
+export default App;
