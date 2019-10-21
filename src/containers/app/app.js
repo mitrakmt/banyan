7477fba00a44e6eabcb8a5f249a703fc1171bf1c
Michael Mitrakos <mike.mitrakos@gmail.com>
2019-10-20 21:37:16 -0500

    feat/header: complete header

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from '../../logo.svg';
+import Header from '../../components/header/header';
 import './app.css';
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <h3>This is the home page broseidon.</h3>
     </div>
   );
