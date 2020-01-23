ac6503fa92f09bb7c7459ae50a840e139507a499
Michael Mitrakos <mike.mitrakos@gmail.com>
2020-01-22 21:40:45 -0600

    fix/faqDesign: update design

--- src/containers/faq/components/question/question.js
+++ src/containers/faq/components/question/question.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./question.scss";
 
 function Question({ question }) {
-
+  const [hidden, setHidden] = useState(false);
+  const toggleHidden = () => {
+    setHidden(!hidden)
+  }
+  
   return (
-    <div className="question">
-      <h5 className="question-header">{question.questionText}</h5>
-      <p className="question-answer">{question.answerText}</p>
+    <div className="question" onClick={toggleHidden}>
+      <div className="question-flex">
+        <h5 className="question-arrow" style={hidden ? { transform: 'rotate(90deg) scale(1,1.3)' } : {}}>></h5>
+        <h5 className="question-header">{question.questionText}</h5>
+      </div>
+      {
+        hidden &&
+        <p className="question-answer">{question.answerText}</p>
+      }
     </div>
   );
 }
