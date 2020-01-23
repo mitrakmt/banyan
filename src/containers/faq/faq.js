f5e43b94160a625fde17babd6adfa3d05fecb4c1
Mike Flores <mike.flores@verys.com>
2020-01-22 21:48:35 -0600

    feat: faq update

--- src/containers/faq/faq.js
+++ src/containers/faq/faq.js
@@ -10,20 +10,12 @@ function Faq() {
       answerText: "Michael Flores and Michael Mitrakos have been professional software engineers for over 4 years. They've held diverse roles for startups and enterprises, and have experience interviewing and hiring dozens of engineers."
     },
     {
-      questionText: "Do you guarantee finding a job?",
-      answerText: "We're going to charge you a boat load so we can make good money and live the dream."
-    },
-    {
       questionText: "How will you help me land a job?",
       answerText: "We're going to charge you a boat load so we can make good money and live the dream."
     },
     {
       questionText: "How can you help me fund the cost of tuition?",
       answerText: "While we can't offer student loans, many bootcamp students choose to fund their tuition via a personal loan, or platforms like Upstart and GoFundMe."
-    },
-    {
-      questionText: "How do I prepare for the admissions test?",
-      answerText: "? Do we know what this is yet.."
     },
     {
       questionText: "How are you different from other bootcamps?",
