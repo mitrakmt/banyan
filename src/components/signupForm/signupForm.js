14511ed64aed50d807e139f8fd2ec36cb7920d2c
Michael Mitrakos <mike.mitrakos@gmail.com>
2020-02-07 10:35:18 -0600

    feat/finalUpdates: minor updates

--- src/components/signupForm/signupForm.js
+++ src/components/signupForm/signupForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MailchimpForm from './components/mailchimpForm';
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 
@@ -23,15 +23,15 @@ function SignupForm() {
       <h2 className="signupForm-header">Transform Your Life</h2>
       <p className="signupForm-description">Banyan Code Camp is designed to produce craftsman who love to write well architected code. We don't just teach you the skills, we help you to find a passion in creating great software. We'll help you find your purpose by creating a learning environment in one of the most beautiful islands in the world.</p>
       <p className="signupForm-description">In the coming months we'll be releasing the cirriculum, schedule, experience agenda, and class dates. So leave your info and we'll reach out to you with next steps!</p>
-      <input className="signupForm-formInput" value={name} onChange={(event) => setName(event.target.value)} placeholder="Name" />
+      <input className="signupForm-formInput" type="name" value={name} onChange={(event) => setName(event.target.value)} placeholder="Name" />
       {
         nameError && <p className="signupForm-error">Name is required</p>
       }
-      <input className="signupForm-formInput" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
+      <input className="signupForm-formInput" type="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
       {
         emailError && <p className="signupForm-error">A valid email is required</p>
       }
-      <input className="signupForm-formInput" value={phone} onChange={(event) => setPhone(event.target.value)} placeholder="Phone Number" />
+      <input className="signupForm-formInput" value={phone} type="tel" onChange={(event) => setPhone(event.target.value)} placeholder="Phone Number" />
       <MailchimpSubscribe
           url={url}
           render={({ subscribe, status, message }) => (
